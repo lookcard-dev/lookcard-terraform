d0280e9563b588639d36b0b7af10c357e0a6cd56
adian <chiuchingwong@gmail.com>
2024-08-22 16:42:13 +0800

    git update latest

--- modules/application/cloudmap.tf
+++ modules/application/cloudmap.tf
@@ -3,10 +3,10 @@ resource "aws_service_discovery_private_dns_namespace" "lookcardlocal_namespace"
   vpc  = var.network.vpc
 }
 
-resource "aws_service_discovery_private_dns_namespace" "api_lookcardlocal_namespace" {
-  name = "api.lookcard.local"
-  vpc  = var.network.vpc
-}
+# resource "aws_service_discovery_private_dns_namespace" "api_lookcardlocal_namespace" {
+#   name = "api.lookcard.local"
+#   vpc  = var.network.vpc
+# }
 
 # resource "aws_service_discovery_service" "xray_daemon_service" {
 #   name = "xray.daemon"
