a2624614c00b30a334c5ef1faf0f3fc997feccb4
adian <chiuchingwong@gmail.com>
2024-08-22 18:23:22 +0800

    feat: pull latest code

--- modules/application/cloudmap.tf
+++ modules/application/cloudmap.tf
@@ -2,22 +2,3 @@ resource "aws_service_discovery_private_dns_namespace" "lookcardlocal_namespace"
   name = "lookcard.local"
   vpc  = var.network.vpc
 }
-
-# resource "aws_service_discovery_private_dns_namespace" "api_lookcardlocal_namespace" {
-#   name = "api.lookcard.local"
-#   vpc  = var.network.vpc
-# }
-
-# resource "aws_service_discovery_service" "xray_daemon_service" {
-#   name = "xray.daemon"
-#   dns_config {
-#     namespace_id = aws_service_discovery_private_dns_namespace.lookcardlocal_namespace.id
-#     dns_records {
-#       ttl  = 10
-#       type = "A"
-#     }
-#   }
-#   health_check_custom_config {
-#     failure_threshold = 1
-#   }
-# }
