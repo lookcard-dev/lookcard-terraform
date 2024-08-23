2eaac995f19ba8bf966f042c3d15edb919f8c4cc
Scott Li <fatjorqaq@gmail.com>
2024-08-23 17:40:54 +0800

    Create ECS notification service v2

---
+++ modules/application/notification_service_v2/security-group.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "Notification_v2" {
+  depends_on  = [var.network]
+  name        = "Notification-v2-Service-Security-Group"
+  description = "Security group for ECS services"
+  vpc_id      = var.network.vpc
+
+  ingress {
+    from_port   = 3001
+    to_port     = 3001
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    # security_groups = [aws_security_group.ALB_SG.id]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    # security_groups = [aws_security_group.ALB_SG.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Notification-v2-Serurity-Group"
+  }
+}
