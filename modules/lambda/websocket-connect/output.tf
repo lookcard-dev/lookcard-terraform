b76d5eea99c092a6cf9d0476948b55f642a69b18
Scott Li <fatjorqaq@gmail.com>
2024-11-27 13:28:24 +0800

    Delete lambda module

--- modules/lambda/websocket-connect/output.tf
+++
@@ -1,7 +0,0 @@
-output "websocket_connect_arn" {
-  value = aws_lambda_function.websocket_connect.invoke_arn
-}
-
-output "websocket_connect_name" {
-  value = aws_lambda_function.websocket_connect.function_name
-}
