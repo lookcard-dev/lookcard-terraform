5a6c06a930915fb26d2b46e520ea365bbfc666b8
Scott Li <fatjorqaq@gmail.com>
2024-12-02 13:57:55 +0800

    Delete old ECS cluster and provider

--- modules/application/cluster.tf
+++ modules/application/cluster.tf
@@ -1,30 +1,5 @@
-
-resource "aws_ecs_cluster" "application" {
-  name = "application"
-  setting {
-    name  = "containerInsights"
-    value = var.ecs_cluster_config.enable ? "enabled" : "disabled"
-  }
-}
-
 resource "aws_ecs_cluster" "listener" {
   name = "listener"
-  setting {
-    name  = "containerInsights"
-    value = var.ecs_cluster_config.enable ? "enabled" : "disabled"
-  }
-}
-
-resource "aws_ecs_cluster" "admin" {
-  name = "admin"
-  setting {
-    name  = "containerInsights"
-    value = var.ecs_cluster_config.enable ? "enabled" : "disabled"
-  }
-}
-
-resource "aws_ecs_cluster" "aml_tron" {
-  name = "aml_tron"
   setting {
     name  = "containerInsights"
     value = var.ecs_cluster_config.enable ? "enabled" : "disabled"
@@ -79,42 +54,9 @@ resource "aws_ecs_capacity_provider" "ec2_amd64_on_demand" {
   }
 }
 
-resource "aws_ecs_cluster_capacity_providers" "application" {
-  cluster_name       = aws_ecs_cluster.application.name
-  capacity_providers = ["FARGATE", "FARGATE_SPOT"]
-  default_capacity_provider_strategy {
-    base              = 1
-    weight            = 100
-    capacity_provider = "FARGATE_SPOT"
-  }
-}
-
 resource "aws_ecs_cluster_capacity_providers" "listener" {
   cluster_name       = aws_ecs_cluster.listener.name
   capacity_providers = ["FARGATE", "FARGATE_SPOT", aws_ecs_capacity_provider.ec2_amd64_on_demand.name, aws_ecs_capacity_provider.ec2_arm64_on_demand.name]
-  default_capacity_provider_strategy {
-    base              = 1
-    weight            = 100
-    capacity_provider = "FARGATE_SPOT"
-  }
-}
-
-resource "aws_ecs_cluster_capacity_providers" "admin" {
-
-  cluster_name       = aws_ecs_cluster.admin.name
-  capacity_providers = ["FARGATE", "FARGATE_SPOT"]
-
-  default_capacity_provider_strategy {
-    base              = 1
-    weight            = 100
-    capacity_provider = "FARGATE_SPOT"
-  }
-}
-
-resource "aws_ecs_cluster_capacity_providers" "aml_tron" {
-
-  cluster_name       = aws_ecs_cluster.aml_tron.name
-  capacity_providers = ["FARGATE_SPOT"]
   default_capacity_provider_strategy {
     base              = 1
     weight            = 100
