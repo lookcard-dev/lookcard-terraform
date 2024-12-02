75cd0eb62756dad82c09ff44920d99e6c9c292d7
Scott Li <fatjorqaq@gmail.com>
2024-12-02 13:11:52 +0800

    Update Config_API-Config_Data configuration

--- modules/application/data-api/dynamodb.tf
+++ modules/application/data-api/dynamodb.tf
@@ -1,8 +1,6 @@
 resource "aws_dynamodb_table" "data_api_data" {
   name           = "Data_API-Data"
   billing_mode   = "PAY_PER_REQUEST"
-  # read_capacity  = 1
-  # write_capacity = 1
   hash_key       = "id"
 
   attribute {
