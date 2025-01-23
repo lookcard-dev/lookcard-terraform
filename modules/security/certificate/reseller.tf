ef19aee8d7bdbec678137bc7ea693c9404d17e5a
Mike Ng <iam@mikeng.io>
2025-01-23 17:00:20 +0800

    feat: Added certificate for `reseller`

---
+++ modules/security/certificate/reseller.tf
@@ -0,0 +1,26 @@
+resource "aws_acm_certificate" "reseller" {
+  provider          = aws.east
+  domain_name       = "reseller.${var.general_domain}"
+  subject_alternative_names = ["*.reseller.${var.general_domain}"]
+  validation_method = "DNS"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "reseller" {
+  provider        = aws.east
+  allow_overwrite = true
+  name            = tolist(aws_acm_certificate.reseller.domain_validation_options)[0].resource_record_name
+  records         = [tolist(aws_acm_certificate.reseller.domain_validation_options)[0].resource_record_value]
+  type            = tolist(aws_acm_certificate.reseller.domain_validation_options)[0].resource_record_type
+  zone_id         = data.aws_route53_zone.general_hosted_zone.zone_id
+  ttl             = 360
+}
+
+resource "aws_acm_certificate_validation" "app" {
+  provider                = aws.east
+  certificate_arn         = aws_acm_certificate.reseller.arn
+  validation_record_fqdns = [aws_route53_record.reseller.fqdn]
+}
+  
--- modules/security/main.tf
+++ modules/security/main.tf
@@ -3,3 +3,7 @@ module "waf" {
   waf_logging_s3_bucket = var.waf_logging_s3_bucket
   # reseller_api_stage = var.reseller_api_stage
 }
+
+module "certificate"{
+  source = "certificate"
+}
