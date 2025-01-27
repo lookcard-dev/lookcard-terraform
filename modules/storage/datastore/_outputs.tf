f51b4bbeaba61dec152dcff94ba6bc420ccda264
Mike Ng <iam@mikeng.io>
2025-01-28 04:51:39 +0800

    fix: Null value for non-production database endpoint

--- modules/storage/datastore/_outputs.tf
+++ modules/storage/datastore/_outputs.tf
@@ -1,7 +1,7 @@
 output "writer_endpoint" {
-  value = var.runtime_environment == "production" ? aws_db_proxy.this[0].endpoint : null
+  value = var.runtime_environment == "production" ? aws_db_proxy.this[0].endpoint : aws_rds_cluster.cluster.endpoint
 }
 
 output "reader_endpoint" {
-  value = var.runtime_environment == "production" ? aws_db_proxy_endpoint.readonly[0].endpoint : null
+  value = var.runtime_environment == "production" ? aws_db_proxy_endpoint.readonly[0].endpoint : aws_rds_cluster.cluster.reader_endpoint
 }
