84e81532e39bb6b41f1fad2de8d7f1c557cde5fb
Mike Ng <iam@mikeng.io>
2025-01-29 08:37:15 +0800

    feat: Implemented Valkey serverless

--- modules/storage/datacache/elasticache.tf
+++ modules/storage/datacache/elasticache.tf
@@ -41,11 +41,23 @@ resource "aws_elasticache_replication_group" "cluster" {
   }
 }
 
-# resource "aws_elasticache_serverless_cache" "this" {
+# resource "aws_elasticache_serverless_cache" "cluster" {
 #   name = "datacache"
 #   engine = "valkey"
-#   major_engine_version = "7.0"
-  
+#   major_engine_version = "8"
+
+#   subnet_ids = var.subnet_ids
+#   security_group_ids = [aws_security_group.cluster_security_group.id]
+
+#   cache_usage_limits {
+#     data_storage {
+#         maximum = var.runtime_environment == "production" ? 10 : 1
+#         unit = "GB"
+#     }
+#     ecpu_per_second {
+#       maximum = var.runtime_environment == "production" ? 5000 : 1000
+#     }
+#   }
 #   tags = {
 #     Environment = var.runtime_environment
 #   }
