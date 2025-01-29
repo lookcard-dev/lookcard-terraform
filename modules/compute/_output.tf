6cb77455be1f1beda65b49494b0db235de42744a
Mike Ng <iam@mikeng.io>
2025-01-29 22:16:18 +0800

    fix: Connection issue between `crypto-listener` and `crypto-api`

--- modules/compute/_output.tf
+++ modules/compute/_output.tf
@@ -2,6 +2,10 @@ output "listener_cluster_id"{
     value = module.ecs.listener_cluster_id
 }
 
+output "listener_security_group_id"{
+    value = module.ecs.listener_security_group_id
+}
+
 output "core_application_cluster_id"{
     value = module.ecs.core_application_cluster_id
 }
--- modules/compute/ecs/_output.tf
+++ modules/compute/ecs/_output.tf
@@ -2,6 +2,10 @@ output "listener_cluster_id"{
     value = aws_ecs_cluster.listener.id
 }
 
+output "listener_security_group_id"{
+    value = aws_security_group.ec2_cluster_security_group.id
+}
+
 output "core_application_cluster_id"{
     value = aws_ecs_cluster.core_application.id
 }
