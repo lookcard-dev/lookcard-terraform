031225614edfc6c50c0b0b64000f4171a38e03e2
Mike Ng <iam@mikeng.io>
2025-02-17 14:53:02 +0800

    fix: Missing security group

--- modules/application/main.tf
+++ modules/application/main.tf
@@ -1,68 +1,68 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source                = "hashicorp/aws"
       configuration_aliases = [aws.dns]
     }
   }
 }
 
 module "xray-daemon" {
-  source = "./00-xray-daemon"
-  aws_provider = var.aws_provider
-  name = "xray-daemon"
+  source              = "./00-xray-daemon"
+  aws_provider        = var.aws_provider
+  name                = "xray-daemon"
   runtime_environment = var.runtime_environment
-  cluster_id = var.cluster_ids.core_application
-  namespace_id = var.namespace_id
-  network = var.network
+  cluster_id          = var.cluster_ids.core_application
+  namespace_id        = var.namespace_id
+  network             = var.network
 }
 
 module "profile-api" {
-  source = "./01-profile-api"
-  aws_provider = var.aws_provider
-  name = "profile-api"
-  image_tag = var.components["profile-api"].image_tag
-  runtime_environment = var.runtime_environment
-  cluster_id = var.cluster_ids.core_application
-  namespace_id = var.namespace_id
-  network = var.network
+  source                      = "./01-profile-api"
+  aws_provider                = var.aws_provider
+  name                        = "profile-api"
+  image_tag                   = var.components["profile-api"].image_tag
+  runtime_environment         = var.runtime_environment
+  cluster_id                  = var.cluster_ids.core_application
+  namespace_id                = var.namespace_id
+  network                     = var.network
   allow_to_security_group_ids = []
 }
 
 module "data-api" {
-  source = "./02-data-api"
-  aws_provider = var.aws_provider
-  name = "data-api"
-  image_tag = var.components["data-api"].image_tag
-  runtime_environment = var.runtime_environment
-  cluster_id = var.cluster_ids.core_application
-  namespace_id = var.namespace_id
-  network = var.network
+  source                      = "./02-data-api"
+  aws_provider                = var.aws_provider
+  name                        = "data-api"
+  image_tag                   = var.components["data-api"].image_tag
+  runtime_environment         = var.runtime_environment
+  cluster_id                  = var.cluster_ids.core_application
+  namespace_id                = var.namespace_id
+  network                     = var.network
   allow_to_security_group_ids = []
-  kms_key_arns = var.kms_key_arns
+  kms_key_arns                = var.kms_key_arns
 }
 
 module "config-api" {
-  source = "./03-config-api"
-  aws_provider = var.aws_provider
-  name = "config-api"
-  image_tag = var.components["config-api"].image_tag
-  runtime_environment = var.runtime_environment
-  cluster_id = var.cluster_ids.core_application
-  namespace_id = var.namespace_id
-  network = var.network
+  source                      = "./03-config-api"
+  aws_provider                = var.aws_provider
+  name                        = "config-api"
+  image_tag                   = var.components["config-api"].image_tag
+  runtime_environment         = var.runtime_environment
+  cluster_id                  = var.cluster_ids.core_application
+  namespace_id                = var.namespace_id
+  network                     = var.network
   allow_to_security_group_ids = []
 }
 
 module "user-api" {
-  source = "./04-user-api"
-  aws_provider = var.aws_provider
-  name = "user-api"
-  image_tag = var.components["user-api"].image_tag
+  source              = "./04-user-api"
+  aws_provider        = var.aws_provider
+  name                = "user-api"
+  image_tag           = var.components["user-api"].image_tag
   runtime_environment = var.runtime_environment
-  cluster_id = var.cluster_ids.core_application
-  namespace_id = var.namespace_id
-  network = var.network
+  cluster_id          = var.cluster_ids.core_application
+  namespace_id        = var.namespace_id
+  network             = var.network
   allow_to_security_group_ids = [
     module.profile-api.security_group_id,
     module.data-api.security_group_id
@@ -72,52 +72,53 @@ module "user-api" {
 }
 
 module "account-api" {
-  source = "./05-account-api"
-  aws_provider = var.aws_provider
-  name = "account-api"
-  image_tag = var.components["account-api"].image_tag
+  source              = "./05-account-api"
+  aws_provider        = var.aws_provider
+  name                = "account-api"
+  image_tag           = var.components["account-api"].image_tag
   runtime_environment = var.runtime_environment
-  cluster_id = var.cluster_ids.core_application
-  namespace_id = var.namespace_id
-  network = var.network
+  cluster_id          = var.cluster_ids.core_application
+  namespace_id        = var.namespace_id
+  network             = var.network
   allow_to_security_group_ids = [
     module.config-api.security_group_id,
     module.profile-api.security_group_id,
     module.data-api.security_group_id,
-    module.user-api.security_group_id
+    module.user-api.security_group_id,
+    module.crypto-api.security_group_id
   ]
   datacache = var.datacache
   datastore = var.datastore
 }
 
 module "crypto-api" {
-  source = "./06-crypto-api"
-  aws_provider = var.aws_provider
-  name = "crypto-api"
-  image_tag = var.components["crypto-api"].image_tag
+  source              = "./06-crypto-api"
+  aws_provider        = var.aws_provider
+  name                = "crypto-api"
+  image_tag           = var.components["crypto-api"].image_tag
   runtime_environment = var.runtime_environment
-  cluster_id = var.cluster_ids.core_application
-  namespace_id = var.namespace_id
-  network = var.network
+  cluster_id          = var.cluster_ids.core_application
+  namespace_id        = var.namespace_id
+  network             = var.network
   allow_to_security_group_ids = [
     module.config-api.security_group_id,
     module.profile-api.security_group_id,
     module.data-api.security_group_id,
     module.account-api.security_group_id
   ]
-  datacache = var.datacache
-  datastore = var.datastore
+  datacache    = var.datacache
+  datastore    = var.datastore
   kms_key_arns = var.kms_key_arns
 }
 
 module "crypto-listener" {
-  source = "./07-crypto-listener"
-  aws_provider = var.aws_provider
-  name = "crypto-listener"
-  image_tag = var.components["crypto-listener"].image_tag
+  source              = "./07-crypto-listener"
+  aws_provider        = var.aws_provider
+  name                = "crypto-listener"
+  image_tag           = var.components["crypto-listener"].image_tag
   runtime_environment = var.runtime_environment
-  cluster_id = var.cluster_ids.listener
-  network = var.network
+  cluster_id          = var.cluster_ids.listener
+  network             = var.network
   allow_to_security_group_ids = [
     module.crypto-api.security_group_id
   ]
@@ -126,12 +127,12 @@ module "crypto-listener" {
 }
 
 module "crypto-processor" {
-  source = "./08-crypto-processor"
-  aws_provider = var.aws_provider
-  name = "crypto-processor"
-  image_tag = var.components["crypto-processor"].image_tag
+  source              = "./08-crypto-processor"
+  aws_provider        = var.aws_provider
+  name                = "crypto-processor"
+  image_tag           = var.components["crypto-processor"].image_tag
   runtime_environment = var.runtime_environment
-  network = var.network
+  network             = var.network
   allow_to_security_group_ids = [
     module.crypto-api.security_group_id,
     module.account-api.security_group_id,
@@ -141,28 +142,28 @@ module "crypto-processor" {
 }
 
 module "authentication-api" {
-  source = "./09-authentication-api"
-  aws_provider = var.aws_provider
-  name = "authentication-api"
-  image_tag = var.components["authentication-api"].image_tag
+  source              = "./09-authentication-api"
+  aws_provider        = var.aws_provider
+  name                = "authentication-api"
+  image_tag           = var.components["authentication-api"].image_tag
   runtime_environment = var.runtime_environment
-  cluster_id = var.cluster_ids.core_application
-  namespace_id = var.namespace_id
-  network = var.network
+  cluster_id          = var.cluster_ids.core_application
+  namespace_id        = var.namespace_id
+  network             = var.network
   allow_to_security_group_ids = [
     module.profile-api.security_group_id
   ]
 }
 
 module "verification-api" {
-  source = "./10-verification-api"
-  aws_provider = var.aws_provider
-  name = "verification-api"
-  image_tag = var.components["verification-api"].image_tag
+  source              = "./10-verification-api"
+  aws_provider        = var.aws_provider
+  name                = "verification-api"
+  image_tag           = var.components["verification-api"].image_tag
   runtime_environment = var.runtime_environment
-  cluster_id = var.cluster_ids.core_application
-  namespace_id = var.namespace_id
-  network = var.network
+  cluster_id          = var.cluster_ids.core_application
+  namespace_id        = var.namespace_id
+  network             = var.network
   allow_to_security_group_ids = [
     module.data-api.security_group_id,
   ]
@@ -171,76 +172,76 @@ module "verification-api" {
 }
 
 module "notification-api" {
-  source = "./11-notification-api"
-  aws_provider = var.aws_provider
-  name = "notification-api"
-  image_tag = var.components["notification-api"].image_tag
-  runtime_environment = var.runtime_environment
-  cluster_id = var.cluster_ids.core_application
-  namespace_id = var.namespace_id
-  network = var.network
+  source                      = "./11-notification-api"
+  aws_provider                = var.aws_provider
+  name                        = "notification-api"
+  image_tag                   = var.components["notification-api"].image_tag
+  runtime_environment         = var.runtime_environment
+  cluster_id                  = var.cluster_ids.core_application
+  namespace_id                = var.namespace_id
+  network                     = var.network
   allow_to_security_group_ids = []
 }
 
 module "referral-api" {
-  source = "./12-referral-api"
-  aws_provider = var.aws_provider
-  name = "referral-api"
-  image_tag = var.components["referral-api"].image_tag
-  runtime_environment = var.runtime_environment
-  cluster_id = var.cluster_ids.core_application
-  namespace_id = var.namespace_id
-  network = var.network
+  source                      = "./12-referral-api"
+  aws_provider                = var.aws_provider
+  name                        = "referral-api"
+  image_tag                   = var.components["referral-api"].image_tag
+  runtime_environment         = var.runtime_environment
+  cluster_id                  = var.cluster_ids.core_application
+  namespace_id                = var.namespace_id
+  network                     = var.network
   allow_to_security_group_ids = []
-  datastore = var.datastore
-  datacache = var.datacache
+  datastore                   = var.datastore
+  datacache                   = var.datacache
 }
 
 module "domain-api" {
-  source = "./13-domain-api"
-  aws_provider = var.aws_provider
-  name = "domain-api"
-  image_tag = var.components["domain-api"].image_tag
-  runtime_environment = var.runtime_environment
-  cluster_id = var.cluster_ids.core_application
-  namespace_id = var.namespace_id
-  network = var.network
+  source                      = "./13-domain-api"
+  aws_provider                = var.aws_provider
+  name                        = "domain-api"
+  image_tag                   = var.components["domain-api"].image_tag
+  runtime_environment         = var.runtime_environment
+  cluster_id                  = var.cluster_ids.core_application
+  namespace_id                = var.namespace_id
+  network                     = var.network
   allow_to_security_group_ids = []
 }
 
 module "reseller-api" {
-  source = "./14-reseller-api"
-  aws_provider = var.aws_provider
-  name = "reseller-api"
-  image_tag = var.components["reseller-api"].image_tag
-  runtime_environment = var.runtime_environment
-  cluster_id = var.cluster_ids.composite_application
-  namespace_id = var.namespace_id
-  network = var.network
+  source                      = "./14-reseller-api"
+  aws_provider                = var.aws_provider
+  name                        = "reseller-api"
+  image_tag                   = var.components["reseller-api"].image_tag
+  runtime_environment         = var.runtime_environment
+  cluster_id                  = var.cluster_ids.composite_application
+  namespace_id                = var.namespace_id
+  network                     = var.network
   allow_to_security_group_ids = []
-  datastore = var.datastore
-  datacache = var.datacache
+  datastore                   = var.datastore
+  datacache                   = var.datacache
 }
 
 module "reap-proxy" {
-  source = "./15-reap-proxy"
-  aws_provider = var.aws_provider
-  name = "reap-proxy"
-  image_tag = var.components["reap-proxy"].image_tag
-  runtime_environment = var.runtime_environment
-  cluster_id = var.cluster_ids.core_application
-  namespace_id = var.namespace_id
-  network = var.network
+  source                      = "./15-reap-proxy"
+  aws_provider                = var.aws_provider
+  name                        = "reap-proxy"
+  image_tag                   = var.components["reap-proxy"].image_tag
+  runtime_environment         = var.runtime_environment
+  cluster_id                  = var.cluster_ids.core_application
+  namespace_id                = var.namespace_id
+  network                     = var.network
   allow_to_security_group_ids = []
 }
 
 module "apigw-authorizer" {
-  source = "./16-apigw-authorizer"
-  aws_provider = var.aws_provider
-  name = "apigw-authorizer"
-  image_tag = var.components["apigw-authorizer"].image_tag
+  source              = "./16-apigw-authorizer"
+  aws_provider        = var.aws_provider
+  name                = "apigw-authorizer"
+  image_tag           = var.components["apigw-authorizer"].image_tag
   runtime_environment = var.runtime_environment
-  network = var.network
+  network             = var.network
   allow_to_security_group_ids = [
     module.authentication-api.security_group_id,
     module.profile-api.security_group_id
@@ -248,31 +249,31 @@ module "apigw-authorizer" {
 }
 
 module "crypto-faucet" {
-  count = var.runtime_environment == "develop" ? 1 : 0
-  source = "./17-crypto-faucet"
-  aws_provider = var.aws_provider
-  name = "crypto-faucet"
-  image_tag = var.components["crypto-faucet"].image_tag
-  runtime_environment = var.runtime_environment
-  network = var.network
+  count                       = var.runtime_environment == "develop" ? 1 : 0
+  source                      = "./17-crypto-faucet"
+  aws_provider                = var.aws_provider
+  name                        = "crypto-faucet"
+  image_tag                   = var.components["crypto-faucet"].image_tag
+  runtime_environment         = var.runtime_environment
+  network                     = var.network
   allow_to_security_group_ids = []
-  datacache = var.datacache
+  datacache                   = var.datacache
   providers = {
     aws.dns = aws.dns
   }
 }
 
 module "cronjob" {
-  source = "./18-cronjob"
-  aws_provider = var.aws_provider
-  name = "cronjob"
-  image_tag = var.components["cronjob"].image_tag
-  runtime_environment = var.runtime_environment
-  network = var.network
+  source                      = "./18-cronjob"
+  aws_provider                = var.aws_provider
+  name                        = "cronjob"
+  image_tag                   = var.components["cronjob"].image_tag
+  runtime_environment         = var.runtime_environment
+  network                     = var.network
   allow_to_security_group_ids = []
-  datacache = var.datacache
-  datastore = var.datastore
-  cluster_id = var.cluster_ids.cronjob
+  datacache                   = var.datacache
+  datastore                   = var.datastore
+  cluster_id                  = var.cluster_ids.cronjob
   api_image_tags = {
     account_api = var.components["account-api"].image_tag
   }
