bbe7c49123a57acddcb320c5f2d15f0d9d81d4a8
Mike Ng <iam@mikeng.io>
2025-04-09 02:50:12 +0800

    🚀 Remove unused ECS service configurations for Arbitrum Sepolia in the crypto listener module to streamline resource management.

--- modules/application/07-crypto-listener/service.tf
+++ modules/application/07-crypto-listener/service.tf
@@ -62,22 +62,6 @@ resource "aws_ecs_service" "bsc_testnet_getblock_ecs_service" {
   }
 }
 
-resource "aws_ecs_service" "arbitrum_sepolia_blast_ecs_service" {
-  count           = var.runtime_environment == "develop" || var.runtime_environment == "testing" ? 1 : 0
-  name            = "arbitrum-sepolia_blast"
-  task_definition = aws_ecs_task_definition.arbitrum_sepolia_blast_task_definition[0].arn
-  desired_count   = var.image_tag == "latest" ? 0 : 1
-  cluster         = var.cluster_id
-  capacity_provider_strategy {
-    capacity_provider = "LISTENER_EC2_AMD64"
-    weight            = 1
-  }
-  ordered_placement_strategy {
-    type  = "spread"
-    field = "instanceId"
-  }
-}
-
 resource "aws_ecs_service" "base_sepolia_getblock_ecs_service" {
   count           = var.runtime_environment == "develop" || var.runtime_environment == "testing" ? 1 : 0
   name            = "base-sepolia_getblock"
@@ -130,22 +114,6 @@ resource "aws_ecs_service" "bsc_testnet_publicnode_ecs_service" {
   count           = var.runtime_environment == "develop" || var.runtime_environment == "testing" ? 1 : 0
   name            = "bsc-testnet_publicnode"
   task_definition = aws_ecs_task_definition.bsc_testnet_publicnode_task_definition[0].arn
-  desired_count   = var.image_tag == "latest" ? 0 : 1
-  cluster         = var.cluster_id
-  capacity_provider_strategy {
-    capacity_provider = "LISTENER_EC2_AMD64"
-    weight            = 1
-  }
-  ordered_placement_strategy {
-    type  = "spread"
-    field = "instanceId"
-  }
-}
-
-resource "aws_ecs_service" "arbitrum_sepolia_publicnode_ecs_service" {
-  count           = var.runtime_environment == "develop" || var.runtime_environment == "testing" ? 1 : 0
-  name            = "arbitrum-sepolia_publicnode"
-  task_definition = aws_ecs_task_definition.arbitrum_sepolia_publicnode_task_definition[0].arn
   desired_count   = var.image_tag == "latest" ? 0 : 1
   cluster         = var.cluster_id
   capacity_provider_strategy {
