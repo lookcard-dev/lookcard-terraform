52ca4e2307a5c264b09747a92b1ee6aa9e168957
Mike Ng <iam@mikeng.io>
2025-05-08 04:32:38 +0800

    Increases health check interval
    Increases the health check interval for the user, account, and card APIs from 10 to 30 seconds.
    
    This change aims to reduce unnecessary load on the services and prevent false positives during temporary network hiccups or minor performance fluctuations.

--- modules/application/21-card-api/task-definition.tf
+++ modules/application/21-card-api/task-definition.tf
@@ -38,7 +38,7 @@ resource "aws_ecs_task_definition" "task_definition" {
 
       healthCheck = {
         command     = ["CMD-SHELL", "curl -f http://localhost:8080/healthcheckz || exit 1"]
-        interval    = 10 # seconds between health checks
+        interval    = 30 # seconds between health checks
         timeout     = 5  # health check timeout in seconds
         retries     = 3  # number of retries before marking container unhealthy
         startPeriod = 10 # time to wait before performing first health check
