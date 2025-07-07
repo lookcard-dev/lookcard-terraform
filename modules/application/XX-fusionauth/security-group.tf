df421cd580bafa86e2e567bfb646f9a75352c376
Mike Ng <iam@mikeng.io>
2025-07-07 15:00:23 +0800

    Refactors security group rules for webhook API and FusionAuth
    Removes an empty line in the webhook API security group configuration. Adds a new ingress rule for the FusionAuth security group to allow traffic on port 9011, enhancing the security group setup for application communication.

--- modules/application/XX-fusionauth/security-group.tf
+++ modules/application/XX-fusionauth/security-group.tf
@@ -23,3 +23,18 @@ resource "aws_vpc_security_group_ingress_rule" "application_load_balancer_ingres
     ]
   }
 }
+
+resource "aws_vpc_security_group_ingress_rule" "target_ingress_rules" {
+  count = length(coalesce(var.allow_to_security_group_ids, []))
+
+  security_group_id            = var.allow_to_security_group_ids[count.index]
+  referenced_security_group_id = aws_security_group.security_group.id
+  from_port                    = 9011
+  to_port                      = 9011
+  ip_protocol                  = "tcp"
+  lifecycle {
+    ignore_changes = [
+      referenced_security_group_id
+    ]
+  }
+}
