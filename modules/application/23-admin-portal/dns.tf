bc782dff02126ccadc721b4787fd4ae913298348
Mike Ng <iam@mikeng.io>
2025-07-14 18:33:20 +0800

    Refactor `dns.tf` in `admin-portal` module to comment out DNS record resources
    This commit comments out the `cloudflare_dns_record` resources for the target and certificate validation in the `dns.tf` file. This change is part of a cleanup effort to streamline the configuration and may be revisited in future updates.

--- modules/application/23-admin-portal/dns.tf
+++ modules/application/23-admin-portal/dns.tf
@@ -1,30 +1,30 @@
-resource "cloudflare_dns_record" "target" {
-  depends_on = [aws_apprunner_custom_domain_association.custom_domain]
+# resource "cloudflare_dns_record" "target" {
+#   depends_on = [aws_apprunner_custom_domain_association.custom_domain]
 
-  zone_id = var.domain.admin.zone_id
-  name    = "admin.${var.domain.admin.name}"
-  content = aws_apprunner_custom_domain_association.custom_domain.dns_target
-  type    = "CNAME"
-  ttl     = 1
-  proxied = true
-  comment = "Admin Portal"
-}
+#   zone_id = var.domain.admin.zone_id
+#   name    = "admin.${var.domain.admin.name}"
+#   content = aws_apprunner_custom_domain_association.custom_domain.dns_target
+#   type    = "CNAME"
+#   ttl     = 1
+#   proxied = true
+#   comment = "Admin Portal"
+# }
 
-resource "cloudflare_dns_record" "certificate_validation" {
-  for_each = {
-    for record in aws_apprunner_custom_domain_association.custom_domain.certificate_validation_records : record.name => {
-      name   = record.name
-      record = record.value
-      type   = record.type
-    }
-  }
+# resource "cloudflare_dns_record" "certificate_validation" {
+#   for_each = {
+#     for record in aws_apprunner_custom_domain_association.custom_domain.certificate_validation_records : record.name => {
+#       name   = record.name
+#       record = record.value
+#       type   = record.type
+#     }
+#   }
 
-  zone_id = var.domain.general.zone_id
-  name    = each.value.name
-  content = each.value.record
-  type    = each.value.type
-  ttl     = 3600
-  proxied = false
-  comment = "Admin Portal Certificate Validation"
-}
+#   zone_id = var.domain.general.zone_id
+#   name    = each.value.name
+#   content = each.value.record
+#   type    = each.value.type
+#   ttl     = 3600
+#   proxied = false
+#   comment = "Admin Portal Certificate Validation"
+# }
 
