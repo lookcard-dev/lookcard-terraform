b6e1b0c469c25c335a64af73923be45a0430d632
Mike Ng <iam@mikeng.io>
2025-07-21 20:03:45 +0800

    Adds composite key to DynamoDB table
    Configures a composite key (id and ownerProfileId)
    for the DynamoDB table.
    
    This enhances data retrieval efficiency by allowing
    queries based on both subscription ID and owner ID.

--- modules/application/11-notification-api/dynamodb.tf
+++ modules/application/11-notification-api/dynamodb.tf
@@ -2,9 +2,15 @@ resource "aws_dynamodb_table" "web_push_subscription" {
   name         = "Notification_API-Web_Push_Subscription"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "id"
+  range_key    = "ownerProfileId"
 
   attribute {
     name = "id"
+    type = "S"
+  }
+
+  attribute {
+    name = "ownerProfileId"
     type = "S"
   }
 }
